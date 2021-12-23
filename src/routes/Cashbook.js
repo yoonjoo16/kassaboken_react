f676eb474ef96d8d297732006b2a5ee155e34894
Yoonjoo Park <yoonjoo16@gmail.com>
2021-12-23 21:43:10 +0100

    added selecting year and month

--- src/routes/Cashbook.js
+++ src/routes/Cashbook.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { dbService, storageService } from "fbase";
-import Record from "components/Record2";
 import {
   FormControl,
   Select,
@@ -22,23 +21,69 @@ import {
   Box,
   Container,
   Modal,
+  ToggleButton,
+  ToggleButtonGroup,
 } from "@mui/material";
 import DateAdapter from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DatePicker from "@mui/lab/DatePicker";
 
 import AddIcon from "@mui/icons-material/Add";
-import { positions } from "@mui/system";
 
 const Cashbook = () => {
   const [newUser, setNewUser] = useState("");
   const [newPlace, setNewPlace] = useState("");
   const [newDate, setNewDate] = useState(new Date());
   const [newAmount, setNewAmount] = useState(0);
+
   const [records, setRecords] = useState([]);
+  const [year, setYear] = useState("2021");
+  const [month, setMonth] = useState("12");
+
   const [addingOpen, setAddingOpen] = useState(false);
   const [editingOpen, setEditingOpen] = useState(false);
   const [editingRecordId, setEditingRecordId] = useState("");
+
+  const modalStyle = {
+    position: "absolute",
+    top: "20%",
+    left: "30%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const initStates = () => {
+    setNewUser("");
+    setNewPlace("");
+    setNewDate(new Date());
+    setNewAmount(0);
+    setEditingRecordId("");
+  };
+
+  const getRecords = async () => {
+    const dbRecords = await dbService.collection("cashbook").get();
+    dbRecords.forEach((item) => {
+      const recordObj = {
+        ...item.data(),
+        id: item.id,
+      };
+      setRecords((prev) => [recordObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getRecords();
+    dbService.collection("cashbook").onSnapshot((snapshot) => {
+      const recordArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setRecords(recordArray);
+    });
+  }, []);
+
   const handleOpen = (mod) => {
     if (mod == "adding") {
       setAddingOpen(true);
@@ -55,16 +100,6 @@ const Cashbook = () => {
     initStates();
   };
 
-  const modalStyle = {
-    position: "absolute",
-    top: "20%",
-    left: "30%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
-    boxShadow: 24,
-    p: 4,
-  };
-
   const onSubmit = async (event) => {
     event.preventDefault();
     const newRecord = {
@@ -77,14 +112,6 @@ const Cashbook = () => {
     console.log("added");
     initStates();
     handleClose("adding");
-  };
-
-  const initStates = () => {
-    setNewUser("");
-    setNewPlace("");
-    setNewDate(new Date());
-    setNewAmount(0);
-    setEditingRecordId("");
   };
 
   const onUpdate = async (event) => {
@@ -121,28 +148,6 @@ const Cashbook = () => {
     setNewPlace(value);
   };
 
-  const getRecords = async () => {
-    const dbRecords = await dbService.collection("cashbook").get();
-    dbRecords.forEach((item) => {
-      const recordObj = {
-        ...item.data(),
-        id: item.id,
-      };
-      setRecords((prev) => [recordObj, ...prev]);
-    });
-  };
-
-  useEffect(() => {
-    getRecords();
-    dbService.collection("cashbook").onSnapshot((snapshot) => {
-      const recordArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setRecords(recordArray);
-    });
-  }, []);
-
   const onDeleteClick = async (id) => {
     const ok = window.confirm("Are you sure?");
     if (ok) {
@@ -150,8 +155,57 @@ const Cashbook = () => {
     }
   };
 
+  const selectMonth = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setMonth(value);
+  };
+
+  const selectYear = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setYear(value);
+  };
+
   return (
     <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <FormControl sx={{ m: 1, minWidth: 80 }}>
+          <InputLabel id="selectYear-label">Year</InputLabel>
+          <Select
+            value={year}
+            labelId="selectYear-label"
+            label="selectYear"
+            onChange={selectYear}
+          >
+            <MenuItem value="2021">2021</MenuItem>
+            <MenuItem value="2020">2020</MenuItem>
+            <MenuItem value="2019">2019</MenuItem>
+          </Select>
+        </FormControl>
+        <ToggleButtonGroup
+          value={month}
+          exclusive
+          onChange={selectMonth}
+          aria-label="selectMonth"
+          sx={{ m: 2, minWidth: 80 }}
+        >
+          <ToggleButton value="01">1</ToggleButton>
+          <ToggleButton value="02">2</ToggleButton>
+          <ToggleButton value="03">3</ToggleButton>
+          <ToggleButton value="04">4</ToggleButton>
+          <ToggleButton value="05">5</ToggleButton>
+          <ToggleButton value="06">6</ToggleButton>
+          <ToggleButton value="07">7</ToggleButton>
+          <ToggleButton value="08">8</ToggleButton>
+          <ToggleButton value="09">9</ToggleButton>
+          <ToggleButton value="10">10</ToggleButton>
+          <ToggleButton value="11">11</ToggleButton>
+          <ToggleButton value="12">12</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={8}>
@@ -201,12 +255,10 @@ const Cashbook = () => {
                             <Button
                               onClick={(e) => {
                                 setEditingRecordId(record.id);
-                                console.log(record.id);
                                 setNewUser(record.user);
                                 setNewAmount(record.amount);
                                 setNewDate(record.date.toDate());
                                 setNewPlace(record.place);
-                                console.log(editingRecordId);
                                 handleOpen("editing");
                               }}
                             >
